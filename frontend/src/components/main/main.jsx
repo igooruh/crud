2f00efa0409d5826c6c6c3285b037446e61a5409
Igor Pedro L M Martins <igooruh@icloud.com>
2019-11-16 15:31:05 -0300

    Create component what import the component header

---
+++ frontend/src/components/main/main.jsx
@@ -0,0 +1,12 @@
+import './main.css'
+import React from 'react'
+import Header from '../header/header'
+
+export default props =>
+
+    <React.Fragment>
+        <Header />
+        <main className='content'>
+            conteúdo
+        </main>
+    </React.Fragment>  
