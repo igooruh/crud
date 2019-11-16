bf7f544eb5b9cd8f4828095c677bc5e3c1f23472
Igor Pedro L M Martins <igooruh@icloud.com>
2019-11-16 17:19:20 -0300

    Add image for logo in the project

--- frontend/src/components/logo/logo.jsx
+++ frontend/src/components/logo/logo.jsx
@@ -1,6 +1,11 @@
 import './logo.css'
+import logo from '../../assets/images/logo.png'
 import React from 'react'
 
 export default props => 
 
-    <aside className='logo'>Logo</aside>
+    <aside className='logo'>
+        <a href='/' className='logo'>
+            <img src={ logo } alt='logo' />
+        </a>
+    </aside>
