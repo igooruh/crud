fad3e70ef7fdc212e38befefeb3cff26dd2e6dee
Igor Pedro L M Martins <igooruh@icloud.com>
2019-11-16 22:48:06 -0300

    Add layout in component

--- frontend/src/components/navbar/nav-bar.jsx
+++ frontend/src/components/navbar/nav-bar.jsx
@@ -3,4 +3,14 @@ import React from 'react'
 
 export default props =>
 
-    <aside className='navbar'>Navegação</aside>
+    <aside className='menu-area'>
+        <nav className='menu'>
+            {/* Refactoring at home */}
+            <a href='#/'>
+                <i className='fa fa-home'></i> Início
+            </a>
+            <a href='#/users'>
+                <i className='fa fa-users'></i> Usuários
+            </a>
+        </nav>
+    </aside>
