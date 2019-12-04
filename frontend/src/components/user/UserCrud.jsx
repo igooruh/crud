90acbba74d3f3f2cd926ae213825a0a68420121c
Igor Pedro L M Martins <igooruh@icloud.com>
2019-12-03 23:34:46 -0300

    Create functions to methods put and post

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react'
 import Main from '../main/main'
+import axios from 'axios'
+
+const BaseURL = 'http://localhost:3001/users'
+const initialState = {
+    user: { name: '', email: '' },
+    list: []
+}
 
 const headerProps = {
     icon: 'users',
@@ -8,6 +15,29 @@ const headerProps = {
 }
 
 export default class UserCrud extends Component {
+    state = {...initialState}
+    
+    clear() {
+        this.setState({user: initialState.user})
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${BaseURL}/${user.id}` : BaseURL
+        axios[method](url, user)
+        .then(resp => {
+            const list = this.getUpdateList(resp.data)
+            this.setState({ user: initialState.user, list })
+        })
+    }
+
+    getUpdateList(user) {
+        const list = this.setState.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
     render() {
         return (
             <Main {...headerProps}>
