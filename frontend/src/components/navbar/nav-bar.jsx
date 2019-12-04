57363da1920db66ecb9c14e530fd5469e962d7d1
Igor Pedro L M Martins <igooruh@icloud.com>
2019-12-03 22:08:23 -0300

    Import link router

--- frontend/src/components/navbar/nav-bar.jsx
+++ frontend/src/components/navbar/nav-bar.jsx
@@ -1,16 +1,17 @@
 import './nav-bar.css'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
 
     <aside className='menu-area'>
         <nav className='menu'>
             {/* Refactoring at home */}
-            <a href='#/'>
+            <Link to='/'>
                 <i className='fa fa-home'></i> Início
-            </a>
-            <a href='#/users'>
+            </Link>
+            <Link to='/users'>
                 <i className='fa fa-users'></i> Usuários
-            </a>
+            </Link>
         </nav>
     </aside>
