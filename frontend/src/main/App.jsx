59a577b31250cca9856b007bbc9ef889b7aa20ba
Igor Pedro L M Martins <igooruh@icloud.com>
2019-12-03 22:09:36 -0300

    Add import and component router

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -2,17 +2,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
 import React from 'react'
+import { HashRouter} from 'react-router-dom'
 
 import Logo from '../components/logo/logo'
 import Nav from '../components/navbar/nav-bar'
-import Home from '../components/home/home'
+import Routes from './Routes'
 import Footer from '../components/footer/footer'
 
 export default props =>
 
-    <div className='app'>
-        <Logo />
-        <Home />
-        <Nav />
-        <Footer />
-    </div>
+    <HashRouter>
+        <div className='app'>
+            <Logo />
+            <Nav />
+            <Routes />
+            <Footer />
+        </div>
+    </HashRouter>
